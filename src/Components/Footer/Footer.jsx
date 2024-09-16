77310e090089975e7f1d65a1e1cc6adea9540897
Dev-using-Ansh <kuamrguptaansh0@gmail.com>
2024-09-16 19:52:14 +0530

    Login-signup form completed

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,44 +1,47 @@
 import react from "react";
+import "./Footer.css"
 import { Link } from "react-router-dom";
 import logo from "../Assets/Navbar/logo.png"
 import instagram from "../Assets/Footer/instagram_icon.png"
 import pinterest from "../Assets/Footer/pinterest_icon.png"
-import whatsapp from "../Assets/Footer/whatsapp_icon.png" 
+import whatsapp from "../Assets/Footer/whatsapp_icon.png"
 const Footer = () => {
-    return ( 
-        <div className="footer flex-col text-center ">
-            <div className="flex justify-center items-center my-5">
-                <img src={logo} alt=""/>
-                <div className="footer-title-name flex text-2xl ">
-                    <div className="text-red-600 font-bold ">
-                        Urban
-                    </div>
-                    <div className="text-gray-500 font-extrabold mx-2">
-                        Styling
+    return (
+        <div className="footer flex items-center justify-center bg-orange-100 pt-3">
+            <div>
+                <div className="flex justify-center items-center">
+                    <img src={logo} alt="" />
+                    <div className="footer-title-name flex text-2xl ">
+                        <div className="text-red-600 font-bold ">
+                            Urban
+                        </div>
+                        <div className="text-gray-500 font-extrabold mx-2">
+                            Styling
+                        </div>
                     </div>
                 </div>
-            </div>
-            <ul className="flex justify-center space-x-6 my-5 text-gray-500 text-xl ">
-                <li><Link to="">Company</Link></li>
-                <li><Link to="">Products</Link></li>
-                <li><Link to="">Offices</Link></li>
-                <li><Link to="">About</Link></li>
-                <li><Link to="">Contact</Link></li>
-            </ul>
-            <ul className="flex justify-center space-x-4 my-4 text-gray-500 ">
-                <li><Link to=""><img src={instagram} alt=""></img>
-                </Link></li>
-                <li><Link to=""><img src={pinterest} alt=""></img>
-                </Link></li>
-                <li><Link to=""><img src={whatsapp} alt=""></img>
-                </Link></li>
-            </ul>
-            <div className="copyright text-xs text-gray-500 text-lg mb-10 ">
-            &copy;2024 by UrbanStyling. <br />
-                All Rights Reserved Proudly by MIT Students
+                <ul className="flex justify-center space-x-6 my-5 text-gray-500 text-xl ">
+                    <li><Link to="">Company</Link></li>
+                    <li><Link to="">Products</Link></li>
+                    <li><Link to="">Offices</Link></li>
+                    <li><Link to="">About</Link></li>
+                    <li><Link to="">Contact</Link></li>
+                </ul>
+                <ul className="flex justify-center space-x-4 my-4 text-gray-500 ">
+                    <li><Link to=""><img src={instagram} alt=""></img>
+                    </Link></li>
+                    <li><Link to=""><img src={pinterest} alt=""></img>
+                    </Link></li>
+                    <li><Link to=""><img src={whatsapp} alt=""></img>
+                    </Link></li>
+                </ul>
+                <div className="copyright text-xl border-t-gray-500 text-gray-500 text-lg mb-3 ">
+                    &copy;2024 by UrbanStyling.
+                    All Rights Reserved Proudly by MIT Students
+                </div>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Footer;
--- src/Pages/LoginSignup/LoginSignup.jsx
+++ src/Pages/LoginSignup/LoginSignup.jsx
@@ -1,10 +1,41 @@
 import react from "react"
+import "./LoginSignup.css"
 const LoginSignup = () => {
-    return ( 
-        <h3 className='text-4xl font-bold text-black-500 font-extrabold'>
-            Welcome to the Login-signup page!!!
-        </h3>
-     );
+    return (
+        <form className="form">
+            <div className="form-body space-y-3">
+                <div className="text-center font-semibold text-4xl my-14 h-8">
+                    Sign Up
+                </div>
+                <div className="input-boxes my-3 mx-12 flex-col space-y-4">
+                <input type="text" placeholder="Username" /> <br />    <input type="email"
+                    placeholder="Email" /> <br />
+                <input type="Password"
+                    placeholder="Password" /> <br />
+                </div>
+                <div className="flex pl-11">
+                    <input type="checkbox" className="p-4"/>
+                    <div className="flex font-semibold text-gray-500 ">
+                        <div className="accept ml-2">
+                            I accept the
+                        </div>
+                        <div className="terms-conditions text-orange-600 ml-1">
+                            terms and conditions
+                        </div>
+                    </div>
+                </div>
+                <button className="signup-button ">Sign up</button>
+                <div className="flex font-semibold text-gray-500 pl-9 mb-3 items-center justify-center py-4">
+                    <div>
+                        Already have an account?
+                    </div>
+                    <div className="terms-conditions text-orange-600 ml-1">
+                        Login
+                    </div>
+                </div>
+            </div>
+        </form>
+    );
 }
- 
+
 export default LoginSignup;
