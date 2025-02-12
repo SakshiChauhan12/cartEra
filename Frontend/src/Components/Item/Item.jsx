d093351e478ec46564883a73149049f390d7b060
Dev-using-Ansh <kuamrguptaansh0@gmail.com>
2025-02-12 14:54:39 +0530

    Make the responsive login and signup page

--- Frontend/src/Components/Item/Item.jsx
+++ Frontend/src/Components/Item/Item.jsx
@@ -6,14 +6,14 @@ const Item = ({ product }) => {
     return (
         <div className="item">
                 <Link to={`/product/${product.id}`}>
-                    <div className="item-cards" key={product.id}>
-                        <img onClick={window.scrollTo(0,0)} src={product.image} alt="" className="item-image" />
-                        <h3 className="font-semibold text-sm md:text-xl">{product.name}</h3>
+                    <div className="item-cards p-2 md:p-4" key={product.id}>
+                        <img onClick={window.scrollTo(0,0)} src={product.image} alt="" className="item-image mb-2 md:mb-4" />
+                        <h3 className="font-semibold text-xs md:text-lg">{product.name}</h3>
                         <div className="cost flex space-x-2 justify-center">
-                            <div className="old_cost font-semibold text-xl">
+                            <div className="old_cost font-semibold text-xs md:text-lg">
                                 ₹{product.old_cost}
                             </div>
-                            < div className="new_cost font-semibold text-xl">
+                            < div className="new_cost font-semibold text-xs md:text-lg">
                                 ₹{product.new_cost}
                             </div>
 
